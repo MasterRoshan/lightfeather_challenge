5a3e593cfdff0e9f21868be2d8d88aeb25987a31
MasterRoshan <t22thejohnson@gmail.com>
2022-05-18 17:45:58 -0400

    switched from print to logging
    for easier docker use

--- notify/settings.py
+++ notify/settings.py
@@ -142,3 +142,18 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ]
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from users.serializers import SubmitSerializer, SupervisorListSerializer, SupervisorSelectSerializer
 from users.models import User
 from rest_framework import generics
+import logging
+logger = logging.getLogger(__name__)
 
 class SupervisorList(generics.ListAPIView):
     queryset = User.objects.filter(jurisdiction__regex=r'\D').order_by('jurisdiction', 'last_name', 'first_name')
@@ -14,7 +16,7 @@ class Submit(generics.CreateAPIView):
     serializer_class = SubmitSerializer
     def perform_create(self, serializer):
         data = serializer.validated_data
-        print(f"\n\nSubmitted Data\n"
+        logger.info(f"\n\nSubmitted Data\n"
             f"**************\n\n"
             f"first_name: {data['first_name']}\n"
             f"last_name: {data['last_name']}\n"
